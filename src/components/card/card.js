b68cba25eb9179e98a89c6f976d663fdb4f40714
gojimor <jgthjgth@gmail.com>
2022-12-23 22:20:27 +0300

    верстка пофикшена

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -10,16 +10,18 @@ const Card = ({ title, poster, vote, release, description }) => {
     <Space className="card" size={20}>
       <Image width={183} src={`https://image.tmdb.org/t/p/w200/${poster}`} />
       <Space className="card__body" direction="vertical" size="small">
-        <Space size={110}>
+        <div className="card__title-wrap">
           <h5 className="card__title">{title}</h5>
           <span className="card__digital-rate">{vote}</span>
-        </Space>
+        </div>
         <Text type="secondary">{format(new Date(release), "d MMMM, y")}</Text>
         <Space>
           <Button size="small">Action</Button>
           <Button size="small">Drama</Button>
         </Space>
-        <Text className="card__description">{description}</Text>
+        <Text className="card__description" ellipsis={false}>
+          {description}
+        </Text>
         <Rate allowHalf defaultValue={vote} count={10} />
       </Space>
     </Space>
