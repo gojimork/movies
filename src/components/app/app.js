285e04824ebb3c166bd83d7ee155a248611cf9ac
gojimor <jgthjgth@gmail.com>
2023-02-17 09:59:10 +0300

    add debounce

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,20 +3,24 @@ import Header from "../header";
 import CardList from "../card-list";
 import "./app.css";
 import { Component } from "react";
+import debounce from "lodash.debounce";
 
 export default class App extends Component {
   state = {
     label: "go",
   };
 
-  onLabelChange = (e) => {
+  updateQuary = (e) => {
     this.setState({ label: e.target.value });
+    console.log(1);
   };
+
+  debounceOnChange = debounce(this.updateQuary, 2000);
 
   render() {
     return (
       <div className="app-wrap">
-        <Header onLabelChange={this.onLabelChange} />
+        <Header onLabelChange={this.debounceOnChange} />
         <CardList request={this.state.label} />
         <Pagination defaultCurrent={1} total={50} />
       </div>
