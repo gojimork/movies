072c9ffabfcd6c422d296252addde853a227959a
gojimork <crusejohen@gmail.com>
2023-03-01 14:27:31 +0300

    use className packege

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,6 +2,7 @@ import { Image, Space, Typography, Rate } from "antd";
 import { format } from "date-fns";
 import MovieApiService from "../../services";
 import "./card.css";
+import classNames from "classnames";
 
 const { Text } = Typography;
 const movieApiService = new MovieApiService();
@@ -26,8 +27,13 @@ const Card = ({
   guestSessionId,
   genres,
 }) => {
-  const color =
-    vote < 3 ? "very-bad" : vote < 5 ? "bad" : vote < 7 ? "normal" : "good";
+  const color = classNames(
+    { "": vote },
+    { "very-bad": vote < 3 },
+    { bad: vote >= 3 && vote < 5 },
+    { normal: vote >= 5 && vote < 7 },
+    { good: vote >= 7 }
+  );
   return (
     <Space className="card" size={20}>
       <Image
@@ -37,9 +43,7 @@ const Card = ({
       <Space className="card__body" direction="vertical" size="small">
         <div className="card__title-wrap">
           <h5 className="card__title">{title}</h5>
-          <span className={`card__digital-rate ${vote ? color : ""}`}>
-            {vote}
-          </span>
+          <span className={`card__digital-rate ${color}`}>{vote}</span>
         </div>
         <Text type="secondary">
           {release ? format(new Date(release), "d MMMM, y") : null}
